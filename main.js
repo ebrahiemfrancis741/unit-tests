90c6a7640e3d5dd71c6dc7548a61719c4affcaac
Ebrahiem Francis <ebrahiemfrancis741@gmail.com>
2025-02-17 04:29:32 +0200

    Add test and implementation for reverseString() function

--- main.js
+++ main.js
@@ -2,4 +2,11 @@ function capitalize(text) {
   return text[0].toUpperCase() + text.slice(1);
 }
 
-export { capitalize };
+function reverseString(text){
+  text = text.split("");
+  text = text.reverse();
+  text = text.join("");
+  return text;
+}
+
+export { capitalize, reverseString };
--- main.test.js
+++ main.test.js
@@ -1,7 +1,13 @@
-import { capitalize } from "./main.js";
+import { capitalize, reverseString } from "./main.js";
 
 test("capitalize", () => {
   expect(capitalize("hello")).toEqual("Hello");
   expect(capitalize("bye")).toEqual("Bye");
   expect(capitalize("my name is lol!")).toEqual("My name is lol!");
+});
+
+test("reverseString", () => {
+  expect(reverseString("hello")).toEqual("olleh");
+  expect(reverseString("Hello")).toEqual("olleH");
+  expect(reverseString("Bye!?")).toEqual("?!eyB");
 });
