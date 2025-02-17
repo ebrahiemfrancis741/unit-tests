4e0c0ffad8b402543f4f5fe10a501957063c8c44
Ebrahiem Francis <ebrahiemfrancis741@gmail.com>
2025-02-18 00:51:10 +0200

    Add tests and implementation for calculator object

--- main.js
+++ main.js
@@ -2,11 +2,29 @@ function capitalize(text) {
   return text[0].toUpperCase() + text.slice(1);
 }
 
-function reverseString(text){
+function reverseString(text) {
   text = text.split("");
   text = text.reverse();
   text = text.join("");
   return text;
 }
 
-export { capitalize, reverseString };
+let calculator = {
+  add: (a, b) => {
+    return a + b;
+  },
+
+  subtract: (a, b) => {
+    return a - b;
+  },
+
+  multiply: (a, b) => {
+    return a * b;
+  },
+
+  divide: (a, b) => {
+    return a / b;
+  },
+};
+
+export { capitalize, reverseString, calculator };
--- main.test.js
+++ main.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString } from "./main.js";
+import { capitalize, reverseString, calculator } from "./main.js";
 
 test("capitalize", () => {
   expect(capitalize("hello")).toEqual("Hello");
@@ -10,4 +10,19 @@ test("reverseString", () => {
   expect(reverseString("hello")).toEqual("olleh");
   expect(reverseString("Hello")).toEqual("olleH");
   expect(reverseString("Bye!?")).toEqual("?!eyB");
+});
+
+test("calculator", () => {
+  expect(calculator.add(1, 1)).toEqual(2);
+  expect(calculator.add(-5, 1)).toEqual(-4);
+  expect(calculator.add(27, 0)).toEqual(27);
+  expect(calculator.subtract(1, 1)).toEqual(0);
+  expect(calculator.subtract(-1, 5)).toEqual(-6);
+  expect(calculator.subtract(5, 0)).toEqual(5);
+  expect(calculator.multiply(5, 5)).toEqual(25);
+  expect(calculator.multiply(-1, 5)).toEqual(-5);
+  expect(calculator.multiply(5, 0)).toEqual(0);
+  expect(calculator.divide(5, 5)).toEqual(1);
+  expect(calculator.divide(10, -2)).toEqual(-5);
+  expect(calculator.divide(5, 0)).toEqual(Infinity);
 });
